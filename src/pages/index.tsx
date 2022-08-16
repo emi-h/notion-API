0a66f08c6f06d7bd877578261a1c3aa34e86e443
emih <emi.programming@gmail.com>
2022-08-16 15:45:06 +0900

    ♻️ ref

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import styles from 'src/styles/Home.module.css';
+import { Client } from '@notionhq/client';
+import { NextPage } from 'next';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_KEY });
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const response = await notion.databases.query({
+    database_id: databaseId
+  });
+
+  return {
+    props: {
+      results: response.results,
+    },
+    // revalidate: 10
+  }
+}
+
+const Home:NextPage<QueryDatabaseResponse>=({ results })=> {
+  return (
+    <>
+      <h1 className={styles.siteTtl}>Notion API</h1>
+      <div className={styles.container}>
+      <ul className={styles.list}>
+      {results.map((result) => {
+        return (
+          <li key={result.id}>
+            <h2>{result.properties.Name.title[0].plain_text}</h2>
+            <p>{result.properties.content.rich_text[0].plain_text}</p>
+            <p>slug:<span>{result.properties.slug.rich_text[0].plain_text}</span></p>
+            <p><span>{result.properties.select.select.name}</span></p>
+          </li>
+        )
+      }
+      )}
+    </ul>
+      </div>
+    </>
+  )
+}
+
+export default Home;
